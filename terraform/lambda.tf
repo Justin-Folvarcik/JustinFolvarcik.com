ca4b0695d0ad7ccc5c386ba414cd99fda7c13546
Justin-Folvarcik <jfolvarcik@gmail.com>
2026-05-29 02:45:06 -0400

    Custom tagging for images

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,9 +10,9 @@ data "aws_secretsmanager_secret_version" "jf_com_db_password" {
 resource "aws_lambda_function" "jf_com_main_site" {
   function_name = "jf_com_main_site"
   role          = aws_iam_role.jf_com_admin.arn
-  package_type  = "Image"                                                        # We use Docker images
-  image_uri     = "${aws_ecr_repository.jf_com_main_site.repository_url}:latest" # @TODO: Custom tagging
-  timeout       = 30                                                             # Because we let Aurora spin down to nothing, we need to give it time to warm up
+  package_type  = "Image" # We use Docker images
+  image_uri     = "${aws_ecr_repository.jf_com_main_site.repository_url}:${var.jf_com_tag_random}"
+  timeout       = 30 # Because we let Aurora spin down to nothing, we need to give it time to warm up
   memory_size   = 512
   # ARM is cheaper but don't forget to build with it specifically
   architectures = ["arm64"]
@@ -25,10 +25,10 @@ resource "aws_lambda_function" "jf_com_main_site" {
   environment {
     variables = {
       JF_COM_DJANGO_SECRET_KEY = aws_secretsmanager_secret_version.jf_com_django_secret_key_version.secret_string
-      JF_COM_DB_PASS = jsondecode(data.aws_secretsmanager_secret_version.jf_com_db_password.secret_string)["password"]
-      JF_COM_DB_HOST = aws_rds_cluster.jf_com_main_site_db.endpoint
-      JF_COM_DB_NAME = aws_rds_cluster.jf_com_main_site_db.database_name
-      JF_COM_DEBUG   = lower(var.jf_com_environment) == "production" ? "False" : "True"
+      JF_COM_DB_PASS           = jsondecode(data.aws_secretsmanager_secret_version.jf_com_db_password.secret_string)["password"]
+      JF_COM_DB_HOST           = aws_rds_cluster.jf_com_main_site_db.endpoint
+      JF_COM_DB_NAME           = aws_rds_cluster.jf_com_main_site_db.database_name
+      JF_COM_DEBUG             = lower(var.jf_com_environment) == "production" ? "False" : "True"
     }
   }
 }
@@ -41,11 +41,11 @@ resource "aws_lambda_function_url" "jf_com_main_site_url" {
   # CORS settings for future-proofing
   cors {
     allow_credentials = lower(var.jf_com_environment) == "production" ? true : false
-    allow_origins = lower(var.jf_com_environment) == "production" ? ["https://justinfolvarcik.com"] : ["*"]
-    allow_methods = ["GET", "POST", "HEAD"]
-    allow_headers = ["date", "keep-alive"]
-    expose_headers = ["date", "keep-alive"]
-    max_age = 86400
+    allow_origins     = lower(var.jf_com_environment) == "production" ? ["https://justinfolvarcik.com"] : ["*"]
+    allow_methods     = ["GET", "POST", "HEAD"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["date", "keep-alive"]
+    max_age           = 86400
   }
 }
 
@@ -66,5 +66,5 @@ resource "aws_lambda_permission" "jf_com_lambda_permissions_function" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.jf_com_main_site.function_name
   principal     = "*"
-  statement_id = "AllowPublicInvoke"
+  statement_id  = "AllowPublicInvoke"
 }
